e5395f01a6a35a8d66260a8cebd0778046857d6d
yashr775 <rajy29629@gmail.com>
2024-09-11 14:49:01 +0530

    profile completed

--- Client/src/components/layout/AppLayout.jsx
+++ Client/src/components/layout/AppLayout.jsx
@@ -1,42 +1,62 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable react/display-name */
 
 import Header from "./Header";
 import Title from "../shared/Title";
 import { Grid } from "@mui/material";
 import ChatList from "../specific/ChatList";
+import { samepleChats } from "../../constants/sampleData";
+import { useParams } from "react-router-dom";
+import Profile from "../specific/Profile";
 const AppLayout = (WrappedComponent) => {
     return (props) => {
+        const params = useParams();
+        const chatId = params.chatId;
+
+        const handleDeleteChat = (e, _id, groupChat) => {
+            e.preventDefault();
+            console.log(_id + " " + groupChat)
+        }
+
         return (
             <>
                 <Title title={"Chat App"} description="jklffd" />
                 <Header />
                 <Grid container height={"calc(100vh - 4rem)"} sx={{ width: "100%" }}>
-                    <Grid item
+                    <Grid
+                        item
                         sm={4}
                         md={3}
                         lg={3}
-
                         sx={{
                             display: { xs: "none", sm: "block" },
                         }}
-                        height={"100%"}>
-                        <ChatList />
+                        height={"100%"}
+                    >
+                        <ChatList
+                            chats={samepleChats}
+                            chatId={chatId}
+                            handleDeleteChat={handleDeleteChat}
+                        />
                     </Grid>
                     <Grid item xs={12} sm={8} md={5} lg={5} height={"100%"}>
                         {" "}
                         <WrappedComponent {...props} />
                     </Grid>
-                    <Grid item
+                    <Grid
+                        item
                         md={4}
                         lg={4}
-
                         height={"100%"}
                         sx={{
                             display: { xs: "none", md: "block" },
                             padding: "2rem",
                             bgcolor: "rgba(0,0,0,0.85)",
-                        }}>Third</Grid>
-                </Grid >
+                        }}
+                    >
+                        <Profile />
+                    </Grid>
+                </Grid>
             </>
         );
     };
--- Client/src/constants/sampleData.js
+++ Client/src/constants/sampleData.js
@@ -11,3 +11,21 @@ export const sampleUsers = [
         _id: "2",
     },
 ];
+
+export const samepleChats = [
+    {
+        avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+        name: "John Doe",
+        _id: "1",
+        groupChat: false,
+        members: ["1", "2"],
+    },
+
+    {
+        avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+        name: "John Boi",
+        _id: "2",
+        groupChat: true,
+        members: ["1", "2"],
+    },
+];
