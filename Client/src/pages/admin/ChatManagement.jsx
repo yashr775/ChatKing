3d1d3e57975388188628bf2c0a7278b79521c04a
yashr775 <rajy29629@gmail.com>
2024-09-18 07:19:36 +0530

    chat management completed

--- Client/src/pages/admin/ChatManagement.jsx
+++ Client/src/pages/admin/ChatManagement.jsx
@@ -1,10 +1,77 @@
 import AdminLayout from "../../components/layout/AdminLayout";
+import { Stack, Avatar } from "@mui/material";
+import AvatarCard from "../../components/shared/AvatarCard";
+import { useState } from "react";
+import Table from "../../components/shared/Table";
+const columns = [
+    {
+        field: "id",
+        headerName: "ID",
+        headerClassName: "table-header",
+        width: 200,
+    },
+    {
+        field: "avatar",
+        headerName: "Avatar",
+        headerClassName: "table-header",
+        width: 150,
+        renderCell: (params) => <AvatarCard avatar={params.row.avatar} />,
+    },
+
+    {
+        field: "name",
+        headerName: "Name",
+        headerClassName: "table-header",
+        width: 300,
+    },
+
+    {
+        field: "groupChat",
+        headerName: "Group",
+        headerClassName: "table-header",
+        width: 100,
+    },
+    {
+        field: "totalMembers",
+        headerName: "Total Members",
+        headerClassName: "table-header",
+        width: 120,
+    },
+    {
+        field: "members",
+        headerName: "Members",
+        headerClassName: "table-header",
+        width: 400,
+        renderCell: (params) => (
+            <AvatarCard max={100} avatar={params.row.members} />
+        ),
+    },
+    {
+        field: "totalMessages",
+        headerName: "Total Messages",
+        headerClassName: "table-header",
+        width: 120,
+    },
+    {
+        field: "creator",
+        headerName: "Created By",
+        headerClassName: "table-header",
+        width: 250,
+        renderCell: (params) => (
+            <Stack direction="row" alignItems="center" spacing={"1rem"}>
+                <Avatar alt={params.row.creator.name} src={params.row.creator.avatar} />
+                <span>{params.row.creator.name}</span>
+            </Stack>
+        ),
+    },
+];
 
 const ChatManagement = () => {
+    const [rows, setRows] = useState([]);
     return (
         <AdminLayout>
             {" "}
-            <div>ChatManagement</div>
+            <Table heading={"All Chats"} columns={columns} rows={rows} />
         </AdminLayout>
     );
 };
