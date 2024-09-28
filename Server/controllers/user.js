19dd9e6e665cfcc02b5e40e810d14b6b0f3d4223
yashr775 <rajy29629@gmail.com>
2024-09-28 09:40:56 +0530

    all user routes completed

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -4,6 +4,8 @@ import { cookieOption, sendToken } from "../utils/features.js";
 import { compare } from "bcrypt";
 import { ErrorHandler } from "../utils/utility.js";
 import { Chat } from "../models/chat.js";
+import { Request } from "../models/request.js";
+import { NEW_REQUEST, REFETCH_CHATS } from "../constants/event.js";
 
 const newUser = async (req, res) => {
     const { name, username, password, bio } = req.body;
@@ -20,49 +22,47 @@ const newUser = async (req, res) => {
         bio,
         avatar,
     });
-    sendToken(res, user, 201, "User created")
+    sendToken(res, user, 201, "User created");
 };
 
 const login = TryCatch(async (req, res, next) => {
-
     const { username, password } = req.body;
     const user = await User.findOne({ username }).select("+password");
 
     if (!user) {
-        return next(new ErrorHandler("Invalid credentials", 404))
+        return next(new ErrorHandler("Invalid credentials", 404));
     }
 
     const isMatch = await compare(password, user.password);
     if (!isMatch) {
-        return next(new ErrorHandler("Invalid credentials", 404))
+        return next(new ErrorHandler("Invalid credentials", 404));
     }
-    return sendToken(res, user, 201, `Welcome back ${username}`)
+    return sendToken(res, user, 201, `Welcome back ${username}`);
 });
 
 const getMyProfile = TryCatch(async (req, res, next) => {
-
     const user = await User.findById(req.user);
 
     if (!user) return next(new ErrorHandler("User not found", 404));
     return res.status(200).json({
         success: true,
-        user
-    })
-
-})
+        user,
+    });
+});
 
 const logout = TryCatch(async (req, res) => {
-    return res.status(200).cookie("chatKingToken", "", { ...cookieOption, maxAge: 0 }).json({ message: "Logged out successfully" })
-})
+    return res
+        .status(200)
+        .cookie("chatKingToken", "", { ...cookieOption, maxAge: 0 })
+        .json({ message: "Logged out successfully" });
+});
 
 const searchUser = TryCatch(async (req, res) => {
     const { name = "" } = req.query;
 
     const myChats = await Chat.find({ groupChat: false, members: req.user });
 
-
     const allUsersFromMyChats = myChats.flatMap((chat) => chat.members);
-
 
     const allUsersExceptMeAndFriends = await User.find({
         _id: { $nin: allUsersFromMyChats },
@@ -79,6 +79,141 @@ const searchUser = TryCatch(async (req, res) => {
         success: true,
         users,
     });
-})
+});
 
-export { login, newUser, getMyProfile, logout, searchUser };
+const sendFriendRequest = TryCatch(async (req, res, next) => {
+    const { userId } = req.body;
+
+    const request = await Request.findOne({
+        $or: [
+            { sender: req.user, receiver: userId },
+            { sender: userId, receiver: req.user },
+        ],
+    });
+
+    if (request) return next(new ErrorHandler("Request already sent", 400));
+
+    await Request.create({
+        sender: req.user,
+        receiver: userId,
+    });
+
+    emitEvent(req, NEW_REQUEST, [userId]);
+
+    return res.status(200).json({
+        success: true,
+        message: "Friend Request Sent",
+    });
+});
+
+const acceptFriendRequest = TryCatch(async (req, res, next) => {
+    const { requestId, accept } = req.body;
+
+    const request = await Request.findById(requestId)
+        .populate("sender", "name")
+        .populate("receiver", "name");
+
+    if (!request) return next(new ErrorHandler("Request not found", 404));
+
+    if (request.receiver._id.toString() !== req.user.toString())
+        return next(
+            new ErrorHandler("You are not authorized to accept this request", 401)
+        );
+
+    if (!accept) {
+        await request.deleteOne();
+
+        return res.status(200).json({
+            success: true,
+            message: "Friend Request Rejected",
+        });
+    }
+
+    const members = [request.sender._id, request.receiver._id];
+
+    await Promise.all([
+        Chat.create({
+            members,
+            name: `${request.sender.name}-${request.receiver.name}`,
+        }),
+        request.deleteOne(),
+    ]);
+
+    emitEvent(req, REFETCH_CHATS, members);
+
+    return res.status(200).json({
+        success: true,
+        message: "Friend Request Accepted",
+        senderId: request.sender._id,
+    });
+});
+
+const getMyNotifications = TryCatch(async (req, res) => {
+    const requests = await Request.find({ receiver: req.user }).populate(
+        "sender",
+        "name avatar"
+    );
+
+    const allRequests = requests.map(({ _id, sender }) => ({
+        _id,
+        sender: {
+            _id: sender._id,
+            name: sender.name,
+            avatar: sender.avatar.url,
+        },
+    }));
+
+    return res.status(200).json({
+        success: true,
+        allRequests,
+    });
+});
+
+const getMyFriends = TryCatch(async (req, res) => {
+    const chatId = req.query.chatId;
+
+    const chats = await Chat.find({
+        members: req.user,
+        groupChat: false,
+    }).populate("members", "name avatar");
+
+    const friends = chats.map(({ members }) => {
+        const otherUser = getOtherMember(members, req.user);
+
+        return {
+            _id: otherUser._id,
+            name: otherUser.name,
+            avatar: otherUser.avatar.url,
+        };
+    });
+
+    if (chatId) {
+        const chat = await Chat.findById(chatId);
+
+        const availableFriends = friends.filter(
+            (friend) => !chat.members.includes(friend._id)
+        );
+
+        return res.status(200).json({
+            success: true,
+            friends: availableFriends,
+        });
+    } else {
+        return res.status(200).json({
+            success: true,
+            friends,
+        });
+    }
+});
+
+export {
+    login,
+    newUser,
+    getMyProfile,
+    logout,
+    searchUser,
+    sendFriendRequest,
+    acceptFriendRequest,
+    getMyNotifications,
+    getMyFriends,
+};
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,20 +1,49 @@
-import express from 'express'
-import { getMyProfile, login, logout, newUser, searchUser } from '../controllers/user.js';
-import { singleAvatar } from '../middlewares/multer.js';
-import { isAuthenticated } from '../middlewares/auth.js';
-import { loginValidator, registerValidator, validateHandler } from '../lib/validators.js';
-
-
+import express from "express";
+import {
+    acceptFriendRequest,
+    getMyFriends,
+    getMyNotifications,
+    getMyProfile,
+    login,
+    logout,
+    newUser,
+    searchUser,
+    sendFriendRequest,
+} from "../controllers/user.js";
+import { singleAvatar } from "../middlewares/multer.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import {
+    acceptRequestValidator,
+    loginValidator,
+    registerValidator,
+    sendRequestValidator,
+    validateHandler,
+} from "../lib/validators.js";
 
 const app = express.Router();
 
-app.post("/new", singleAvatar, registerValidator(), validateHandler, newUser)
-app.post("/login", loginValidator(), login)
+app.post("/new", singleAvatar, registerValidator(), validateHandler, newUser);
+app.post("/login", loginValidator(), login);
 
-app.use(isAuthenticated)
-app.get("/me", getMyProfile)
-app.get("/logout", logout)
-app.get("/sendrequest", searchUser)
+app.use(isAuthenticated);
+app.get("/me", getMyProfile);
+app.get("/logout", logout);
+app.get("/search", searchUser);
+app.put(
+    "/sendrequest",
+    sendRequestValidator(),
+    registerValidator,
+    sendFriendRequest
+);
+app.put(
+    "/acceptrequest",
+    acceptRequestValidator(),
+    validateHandler,
+    acceptFriendRequest
+);
+
+app.get("/notifications", getMyNotifications);
+app.get("/friends", getMyFriends);
 
 
 export default app;
