0b61f47179748e96557be7c4c2c8c787c4f562fa
yashr775 <rajy29629@gmail.com>
2024-09-30 11:30:50 +0530

    socket configured in backend

--- Server/app.js
+++ Server/app.js
@@ -1,46 +1,92 @@
-import express from 'express'
-import userRoute from './routes/user.js'
-import chatRoute from './routes/chat.js'
-import adminRoute from './routes/admin.js'
-import { connectDB } from './utils/features.js';
-import dotenv from 'dotenv'
-import { errorMiddleware } from './middlewares/errors.js';
-import cookieParser from 'cookie-parser';
-import { Server } from 'socket.io';
-
-
+import express from "express";
+import userRoute from "./routes/user.js";
+import chatRoute from "./routes/chat.js";
+import adminRoute from "./routes/admin.js";
+import { connectDB } from "./utils/features.js";
+import dotenv from "dotenv";
+import { errorMiddleware } from "./middlewares/errors.js";
+import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { NEW_MESSAGE, NEW_MESSAGE_ALERT, ONLINE_USERS } from "./constants/event.js";
+import { v4 as uuid } from "uuid"
+import { getSockets } from "./lib/helper.js";
+import { Message } from "./models/message.js";
 
 dotenv.config({
-    path: "./.env"
-})
+    path: "./.env",
+});
 
-
-const mongoURI = process.env.MONGO_URI
+const mongoURI = process.env.MONGO_URI;
 const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
 
-
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const adminSecretKey = process.env.ADMIN_SECRET_KEY || "This is secret";
+const userSocketIDs = new Map();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
+connectDB(mongoURI);
 
+app.use("/user", userRoute);
+app.use("/chat", chatRoute);
+app.use("/admin", adminRoute);
 
-connectDB(mongoURI)
+io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
 
+    const user = {
+        _id: "hjafgsajh",
+        name: "kugsdfjasdjahs"
+    }
 
-app.use("/user", userRoute)
-app.use("/chat", chatRoute)
-app.use("/admin", adminRoute)
+    userSocketIDs.set(user._id.toString(), socket.id)
+    console.log(userSocketIDs)
 
+    socket.on(NEW_MESSAGE, async ({ chatId, members, message }) => {
+        const messageForRealTime = {
+            content: message,
+            _id: uuid(),
+            sender: {
+                _id: user._id,
+                name: user.name,
+            },
+            chat: chatId,
+            createdAt: new Date().toISOString(),
+        };
 
-app.use(errorMiddleware)
+        const messageForDB = {
+            content: message,
+            sender: user._id,
+            chat: chatId,
+        };
 
-app.listen(3000, () => {
-    console.log(`App is listening on ${PORT} in ${envMode}`)
-})
+        const membersSocket = getSockets(members, userSocketIDs)
+        io.to(membersSocket).emit(NEW_MESSAGE, { chatId, message: messageForRealTime });
+        io.to(membersSocket).emit(NEW_MESSAGE_ALERT, { chatId })
 
-export { envMode, adminSecretKey }
+        try {
+            await Message.create(messageForDB)
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+    socket.on("disconnect", () => {
+        userSocketIDs.delete(user._id.toString());
+        console.log("User disconnected");
+    });
+});
+
+app.use(errorMiddleware);
+
+server.listen(3000, () => {
+    console.log(`App is listening on ${PORT} in ${envMode}`);
+});
+
+export { envMode, adminSecretKey };
--- Server/lib/helper.js
+++ Server/lib/helper.js
@@ -4,3 +4,9 @@ export const getOtherMember = (members, userId) => {
     return members.find((member) => member._id.toString() !== userId.toString());
 
 }
+
+export const getSockets = (users = []) => {
+    const sockets = users.map((user) => userSocketIDs.get(user.toString()));
+
+    return sockets;
+};
